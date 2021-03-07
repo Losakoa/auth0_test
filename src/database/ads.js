a53067759df560b939ff3174c2b0f8f62aa66d18
Carlos Feliciano <c.feliciano79@gmail.com>
2021-03-07 12:22:43 -0800

    still need to get update to work, but delete, get, and insert are working correctly

--- src/database/ads.js
+++ src/database/ads.js
@@ -2,20 +2,43 @@
 
 const {getDatabase} = require('./mongo')
 const collectionNames = 'ads'
+const {objectID, ObjectID} = require('mongodb')
 
-async function insertAd(ad) {
+
+
+async function insertAd(ad) { // takes an add key value and inserts it into the database
     const database = await getDatabase()
-        const {insertedId} = await database.collection(collectionNames).insertOne(ad)
-        return insertedId
+        const {insertedId} = await database.collection(collectionNames).insertOne(ad) // inserts the new ad into the db
+        return insertedId // returns the associated id with the new db entry
 }
 
-async function getAds() {
+async function getAds() { // this gets the add from the DB, all of thems
     const database = await getDatabase()
     return await database.collection(collectionNames).find({}).toArray()
 
 }
 
+async function deleteAd(id) {
+    const database = await getDatabase()
+    await database.collection(collectionNames).deleteOne({_id: new ObjectID(id),})
+}
+
+async function updateAd(id, ad) {
+    const database = await getDatabase();
+    delete ad._id;
+    await database.collection(collectionName).update(
+      { _id: new ObjectID(id), },
+      {
+        $set: {
+          ...ad,
+        },
+      },
+    );
+  }
+
 module.exports = {
     insertAd,
-    getAds
+    getAds,
+    deleteAd,
+    updateAd
 }
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const helmet = require('helmet')
 const morgan = require('morgan')
 const {startDatabase} = require('./database/mongo');
 const {insertAd, getAds} = require('./database/ads');
+const {deleteAd, updateAd } = require('./database/ads')
 
 // defining the express app
 
@@ -17,10 +18,8 @@ const ads = [
 
 //adding Helmet to exchance your API's security
 app.use(helmet())
-
 //using bodyParser to parse JSON bodyies into JS objects
 app.use(bodyParser.json())
-
 // adding cors for all reuqests and morgan to log http requests
 app.use(cors())
 app.use(morgan('combined'))
@@ -28,7 +27,27 @@ app.use(morgan('combined'))
 //define an endpoint
 app.get('/', async (req, res) => {
     res.send(await getAds());
-  });
+  })
+
+app.post('/', async (req,res) => {
+    const newAd = req.body
+    await insertAd(newAd)
+    res.send({message: 'New ad inserted.'})
+
+})
+
+app.post('/:id', async (req, res) => {
+    await deleteAd(req.params.id)
+    res.send({ message: 'Ad removed.' })
+})
+
+// update an ad via the id 
+app.put('/:id', async (req, res) => {
+    const updatedAd = req.body;
+    await updateAd(req.params.id, updatedAd);
+    res.send({ message: 'Ad updated.' });
+});
+
 
 startDatabase().then(async () => {
     await insertAd({title: 'Hello, now from the in-memory database!'})
